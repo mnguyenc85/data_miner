7bd1d78a74fdd07bfd787f87d528b90b0dd52494
NCM <mnguyenc85@gmail.com>
2024-03-22 09:55:05 +0700

    Bo qua clipboard

--- ffscraper.py
+++ ffscraper.py
@@ -1,10 +1,5 @@
 # Extract data from forexfactory.com
 # Date: 2024/03/21
-# Usage:
-#   Ví dụ: mở https://www.forexfactory.com/calendar?week=mar17.2024
-#   vào developer tools -> network -> https://www.forexfactory.com/calendar?week=mar17.2024
-#   copy toàn bộ
-#   chạy python
 
 import win32clipboard
 import re
@@ -17,12 +12,18 @@ rootdir = './data/'
 timeZone = timedelta(hours=-11)
 oneweek = timedelta(days=7)
 
+def get1stWeek(year: int) -> datetime:
+    # 2000 -> 2030
+    sun = [ 2, 7, 6, 5, 4, 2, 1, 7, 6, 4, 3, 2, 1, 6, 5, 4, 3, 1, 7, 6, 5, 3, 2, 1, 7, 5, 4, 3, 2, 7, 6 ]
+    if year >= 2000 and year <= 2030:
+        return datetime(year=year, month=1, day=sun[year - 2000])
+    return None
+
 def getClipboard() -> str:
     win32clipboard.OpenClipboard()
     data = win32clipboard.GetClipboardData()
     win32clipboard.CloseClipboard()
     return data
-
 
 def findJavascript(s: str):
     """ Trích javascript code từ html """
@@ -37,7 +38,7 @@ def getImpact(s: str) -> str:
     elif s.startswith('Medium'): return 'Medium'
     elif s.startswith('High'): return 'High'
     elif s.startswith('Non'): return 'Non'
-    else: return ''
+    else: return s
     # elif s.startswith('Very high'): return 'Very High'
 
 def convertTime(tstr: str):
@@ -49,7 +50,6 @@ def convertTime(tstr: str):
     else:
         t:datetime = datetime.strptime(tstr, "%I:%M%p")
         return timedelta(hours=t.hour, minutes=t.minute)
-
 
 def convertToDatFile(days):
     """ Chuyen doi tuong trich tu html sang doi tuong dung cho Expert 'trading_news' """
@@ -70,8 +70,8 @@ def convertToDatFile(days):
 
     return events, startOfWeek
 
-def parseNSave():
-    jDays = extractData(getClipboard())
+def parseNSave(s: str):
+    jDays = extractData(s)
     print(f'Number of found blocks: {len(jDays)}')
     for s in jDays:
         days = json.loads(s)
@@ -89,20 +89,20 @@ def parseNSave():
         
         print(f'Save to {fn}')
 
+def parseNSaveCB():
+    parseNSave(getClipboard())
+
 def crawlff(startDate: datetime, endDate: datetime):
     """ startDate must be Sunday """
     while startDate < endDate:
+        print(f'Mine week: {startDate.strftime('%a %Y/%m/%d')}')
         # url = 'https://www.forexfactory.com/calendar?week=feb25.2024'
         url = f'https://www.forexfactory.com/calendar?week={startDate.strftime('%b%d.%Y')}'
         headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' }
         x = requests.get(url, headers=headers)
         if x.status_code == 200:
             txt = x.content.decode()
-            win32clipboard.OpenClipboard()
-            win32clipboard.EmptyClipboard()
-            win32clipboard.SetClipboardText(txt, win32clipboard.CF_TEXT)
-            win32clipboard.CloseClipboard()
-            parseNSave()
+            parseNSave(txt)
         else:
             print(x.status_code)
         startDate = startDate + oneweek
@@ -124,5 +124,5 @@ def crawlff(startDate: datetime, endDate: datetime):
 # crawlff(datetime(year=2020,month=1,day=5), datetime(year=2021,month=1,day=1))
 # crawlff(datetime(year=2021,month=1,day=3), datetime(year=2022,month=1,day=1))
 # crawlff(datetime(year=2022,month=1,day=2), datetime(year=2023,month=1,day=1))
-crawlff(datetime(year=2023,month=1,day=1), datetime(year=2024,month=1,day=1))
-# crawlff(datetime(year=2024,month=1,day=7), datetime(year=2024,month=3,day=21))
+# crawlff(datetime(year=2023,month=1,day=1), datetime(year=2024,month=1,day=1))
+# crawlff(datetime(year=2024,month=1,day=7), datetime(year=2025,month=1,day=1))
